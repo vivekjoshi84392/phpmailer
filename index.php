eb4b885a888c964d215dd6f6e379b39158c12d19
vivekjoshi84392 <82107856+vivekjoshi84392@users.noreply.github.com>
2021-05-21 21:07:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,39 +9,26 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
-//Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->UserName   = 'vivekjoshi84392@gmail.com';                     //SMTP username
-    $mail->Password   = '#Vivek123';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('vivekjoshi84392@gmail.com', 'Mailer');
-    $mail->addAddress('vivekjoshicse@gmail.com');     //Add a recipient
-    $mail->addReplyTo('info@example.com', 'Information');
-  //  $mail->addCC('cc@example.com');
-  //  $mail->addBCC('bcc@example.com');
-
-    //Attachments
-   // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+  $Correo = new PHPMailer();
+  $Correo->IsSMTP();
+  $Correo->SMTPAuth = true;
+  $Correo->SMTPSecure = "tls";
+  $Correo->Host = "smtp.gmail.com";
+  $Correo->Port = 587;
+  $Correo->Username = "vivekjoshi84392@gmail.com";
+  $Correo->Password = "#Vivek123";
+  $Correo->SetFrom('vivekjoshi84392@gmail.com','De Yo');
+  $Correo->FromName = "From";
+  $Correo->AddAddress("vivekjoshicse@gmail.com");
+  $Correo->Subject = "Prueba con PHPMailer";
+  $Correo->Body = "<H3>Bienvenido! Esto Funciona!</H3>";
+  $Correo->IsHTML (true);
+  if (!$Correo->Send())
+  {
+    echo "Error: $Correo->ErrorInfo";
+  }
+  else
+  {
+    echo "Message Sent!";
+  }
+?>
