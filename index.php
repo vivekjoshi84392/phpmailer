60edf979a476f9678565a996886a49e811ff57ed
vivekjoshi84392 <82107856+vivekjoshi84392@users.noreply.github.com>
2021-05-21 21:34:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'vivekjoshi84392@gmail.com';                     //SMTP username
-    $mail->Password   = '#Vivek123';                               //SMTP password
+    $mail->Username   = 'example.com';                     //SMTP username
+    $mail->Password   = 'password';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('vivekjoshi84392@gmail.com', 'Mailer');
-    $mail->addAddress('vivekjoshicse@gmail.com');     //Add a recipient
+    $mail->setFrom('example.com', 'Mailer');
+    $mail->addAddress('example@gmail.com');     //Add a recipient
     $mail->addReplyTo('info@example.com', 'Information');
   //  $mail->addCC('cc@example.com');
   //  $mail->addBCC('bcc@example.com');
