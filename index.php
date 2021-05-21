504f667b197dd24ad0b49020c60b3722cf23df1b
vivekjoshi84392 <82107856+vivekjoshi84392@users.noreply.github.com>
2021-05-21 21:03:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'vivekjoshi84392@gmail.com';                     //SMTP username
+    $mail->UserName   = 'vivekjoshi84392@gmail.com';                     //SMTP username
     $mail->Password   = '#Vivek123';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
